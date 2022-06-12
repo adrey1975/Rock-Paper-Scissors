9b477b7fe304b4e2452fc71e7fc73278703a4224
adrey1975 <reyesadrian34@gmail.com>
2022-06-12 14:49:05 -0500

    Game works, just need to add code to tally score and loop to play multiple rounds

--- script.js
+++ script.js
@@ -6,15 +6,51 @@ function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-console.log(computerPlay());
+
 //---------------------------------------------
 function playerChoice() { 
     let player = prompt('Choose your weapon: ').toLowerCase();
     return player;
 }
-console.log(playerChoice());
 //---------------------------------------------
+function determineWinner(playerChoice, computerPlay) {
+    if (computerPlay === playerChoice) {
+        return "Tie!";
+    }
+    if (playerChoice === 'rock') {
+        if (computerPlay === 'paper') {
+            return "You lose, paper beats rock!";
+        } else {
+            return "Congrats, you win!";
+        }
+    }
 
+    if (playerChoice === 'paper') {
+        if (computerPlay === 'scissors') {
+            return "You lose, scissors beats paper!";
+        } else {
+            return "You win!";
+        }
+    }
 
+    if (playerChoice === 'scissors') {
+        if (computerPlay === 'rock') {
+            return "You lose, rock beats scissors!";
+        } else {
+            return "You win!";
+        }
+    }
+}
+//---------------------------------------------
+function playGame() {
+    const userChoice = playerChoice();
+    const cpuChoice = computerPlay();
+
+    console.log('You chose: ' + userChoice);
+    console.log('Computer chose: ' + cpuChoice);
+
+    console.log(determineWinner(userChoice, cpuChoice));
+}
+playGame();
 
 
