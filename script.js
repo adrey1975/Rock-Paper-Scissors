17ba9977af2f4ce96ba9eaa7cae08c3182fdd962
adrey1975 <reyesadrian34@gmail.com>
2022-06-11 21:21:51 -0500

    Computer play function is working

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-console.log(computerSelection);
+console.log(computerPlay());
 //---------------------------------------------
 function playerChoice() { 
     let player = prompt('Choose your weapon: ').toLowerCase();
     return player;
 }
+
 //---------------------------------------------
 
 
