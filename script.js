e07c63a9f1cdd7e23b2781beef04d504dae65ff7
adrey1975 <reyesadrian34@gmail.com>
2022-06-12 16:50:17 -0500

    Added function to determine final winner after multiple rounds

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var pScore = 0;
+var cScore = 0;
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -9,37 +12,49 @@ function playerChoice() {
 }
 //---------------------------------------------
 function determineWinner(playerChoice, computerPlay) {
-    var pScore = 0;
-    var cScore = 0;
-    console.log(pScore, cScore);
-
     if (computerPlay === playerChoice) {
+        pScore++;
+        cScore++;
         return "Tie!";
+        
     }
     if (playerChoice === 'rock') {
         if (computerPlay === 'paper') {
+            cScore++;
             return "You lose, paper beats rock!";
         } else {
+            pScore++;
             return "Congrats, you win!";
         }
     }
 
     if (playerChoice === 'paper') {
         if (computerPlay === 'scissors') {
+            cScore++;
             return "You lose, scissors beats paper!";
         } else {
+            pScore++;
             return "You win!";
         }
     }
 
     if (playerChoice === 'scissors') {
         if (computerPlay === 'rock') {
+            cScore++;
             return "You lose, rock beats scissors!";
         } else {
+            pScore++;
+            cScore++;
             return "You win!";
         }
     }
 }
+    // function newFunction() {
+    //     var pScore = 0;
+    //     var cScore = 0;
+    //     return { pScore, cScore };
+    //}
+
 //---------------------------------------------
 function playGame() {
 
@@ -48,13 +63,26 @@ function playGame() {
 
     console.log('You chose: ' + userChoice);
     console.log('Computer chose: ' + cpuChoice);
-
+    console.log(pScore, cScore);
     console.log(determineWinner(userChoice, cpuChoice));
 }
+//---------------------------------------------
+function finalWinner() {
+    if(pScore>cScore) {
+        alert("After 3 rounds, you are the final champion! congrats!");
+    } else if (pScore<cScore) {
+        alert("You lose! Computers are the superior beings!");
+    } else {
+        alert("No winner! It/'s a draw!");
+    }
+
+}
+//---------------------------------------------
 function rounds() {
     for (let i = 0; i < 3; i++) {
         playGame(i);
     }
+    finalWinner()
 }
 rounds();
 
