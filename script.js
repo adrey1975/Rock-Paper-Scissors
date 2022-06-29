279cde3aa34f1d0732780f1fa51c1200830c04ef
adrey1975 <reyesadrian34@gmail.com>
2022-06-28 21:39:37 -0500

    Reimplemented function that allows users to play round against computer

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 // Function that will randomize our computers choice
-const computerPlay = () => {
-    const randomNumber = (Math.floor(Math.random() * 3))
-    if(randomNumber === 0){
-        return 'Rock';
-    } else if (randomNumber === 1) {
-        return 'Paper';
-    } else {
-        return 'Scissors';
-    }
+function computerPlay() {
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 console.log(computerPlay());
 
+// Function to play a round of RPS
+function playRound (playerChoice, computerChoice) {
+    if (computerChoice === playerChoice) {
+       // pScore++;
+       // cScore++;
+        return "Tie!";
+        
+    }
+    if (playerChoice === 'ROCK') {
+        if (computerChoice === 'PAPER') {
+           // cScore++;
+            return "You lose, paper beats rock!";
+        } else {
+           // pScore++;
+            return "Congrats, you win!";
+        }
+    }
+
+    if (playerChoice === 'PAPER') {
+        if (computerChoice === 'SCISSORS') {
+         //   cScore++;
+            return "You lose, scissors beats paper!";
+        } else {
+          //  pScore++;
+            return "You win!";
+        }
+    }
+
+    if (playerChoice === 'SCISSORS') {
+        if (computerChoice === 'ROCK') {
+          //  cScore++;
+            return "You lose, rock beats scissors!";
+        } else {
+           // pScore++;
+           // cScore++;
+            return "You win!";
+        }
+    }
+}
+
+const playerChoice = "ROCK";
+const computerChoice = computerPlay();
+console.log(playRound(playerChoice, computerChoice));
 
 
